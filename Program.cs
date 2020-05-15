eeeb3bd4005c21e47bd4450374cf4d846d4b933d
fabricio <fnmamao@gmail.com>
2020-05-15 16:30:34 -0300

    exercicio

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CriancaEducada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Me diga o que você pensa sobre o aluno que não faz a lição de casa: Eu acho que é um bobo, e que vai ficar boboca pra sempre.");
+            Console.WriteLine("");
+            
+            Console.WriteLine("uma criança educada diria: Me diga o que você pensa sobre o aluno que não faz a lição de casa: Eu acho que é um bobo, e que vai ficar boboca pra sempre.".Replace("bobo" , "@#$%" ).Replace("boboca" , "!@#$"));
+        }
+    }
+}
